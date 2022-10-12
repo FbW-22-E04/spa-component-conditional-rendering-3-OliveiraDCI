8128a0365206a71ba27f53b53e22d94699d417b0
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-10-12 14:11:28 +0200

    Exercise concluded.

--- src/Alert.js
+++ src/Alert.js
@@ -1,12 +1,14 @@
-import React from "react"
-const Alert = props => {
-	//add the condition inside this function
-
-	return (
-		<div className="alert alert-danger" role="alert">
-			This is a primary alert-check it out!
-		</div>
-	);
+import React from "react";
+const Alert = (props) => {
+  if (props.show === false) {
+    return null;
+  } else {
+    return (
+      <div className="alert alert-danger" role="alert">
+        {props.text}
+      </div>
+    );
+  }
 };
 
 export default Alert;
